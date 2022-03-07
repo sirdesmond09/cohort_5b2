e4a2a43b62dac4481ae1404e1c03b748407a6521
sirdesmond09 <sirdesmond09@gmail.com>
2022-03-07 15:27:59 +0100

    initial commit

---
+++ first_class.py
@@ -0,0 +1,28 @@
+# my_variable = 5.0 #assign 5 to my_variable
+# # print(my_variable)
+
+# variable_type =type(my_variable)
+# print(variable_type)
+
+
+### OPERATORS
+
+# num = (3+4/5)**2
+# print(num**(1/2))
+
+a= 3
+b=10
+c=9
+
+ans = (-b + (b**2 - (4*a*c))**(1/2))/(2*a)
+
+print(ans)
+
+
+p = 10000
+r = 0.08
+t = 12
+n = 6
+
+com_interest = p * (1+(r/n))**(n*t)
+print(com_interest)
