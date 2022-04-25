da8f709bd5baf8040273b4bdf237d38a0c17b06a
sirdesmond09 <sirdesmond09@gmail.com>
2022-04-25 12:24:24 +0100

    bank app correction

--- twelvt.py
+++ twelvt.py
@@ -1,14 +1,17 @@
 import random
+from datetime import datetime
 
 file = open("customers.txt", "r")
-bank_data = eval(file.read())
+dataset = file.readlines()
+customer, transaction = eval(dataset[0]), eval(dataset[1])
 file.close()
 
-# print(bank_data)
+
+
 
 while True:
     title = "WELCOME TO BANK OF NIGERIA"
-    print(title)
+    # print(title)
     user = int(input("1. Register\n2. Login\n3. Exit\n::>> "))
 
     if user == 1:
@@ -29,22 +32,24 @@ while True:
       user_info["acc_type"] = acc
       user_info["bal"] = balance
       user_info["pin"] = pin
+      user_info['is_active'] = True
       
-      bank_data[account_num] = user_info
+      customer[account_num] = user_info
+      transaction[account_num] = []
       
       print(f"\nHello {name.title()}, your account has been created successfully. Your account number is {account_num}. Please do not disclose your pin to anyone.\nThank you for choosing BON")
-    #   print(bank_data)
+    #   print(customer)
     elif user == 2:
         print("*******LOGIN******")
-        acc_nu = input("Account Number\n:>> ")
+        acc_num = input("Account Number\n:>> ")
         p_num = input("PIN\n:>> ")
         
-        user_data = bank_data.get(acc_nu)
+        user_data = customer.get(acc_num)
         # print(user_data)
-        if user_data and user_data['pin']==p_num:
+        if user_data and user_data['pin']==p_num and user_data['is_active']==True:
             while True:    
               print("****WELCOME****")
-              user_select = input("\n1. Deposit\n2. Withdrawal \n3. Transfer\n4. Check Balance\n5. Exit\n:>> ")
+              user_select = input("\n1. Deposit\n2. Withdrawal \n3. Transfer\n4. Check Balance\n5. See recent transactions\n6. Deactivate Account\n7.Logout\n:>> ")
               if user_select == "1":
                 print("******DEPOSIT******")
                 deposit_amount = int(input("Enter amount you want to deposit\n:>> "))  
@@ -52,7 +57,11 @@ while True:
                     print("Amount must be greater than 0")
                 else:
                  user_data['bal'] += deposit_amount
-                 
+                 trans_record ={"amount":deposit_amount,
+                                "status":"credit",
+                                "action":"deposit",
+                                "trans_date":str(datetime.now())}
+                 transaction[acc_num].append(trans_record)
                  print("\nDeposit Successful!!!\nYour new account balance is $%i\n"%user_data["bal"])
 
               elif user_select == "2":
@@ -63,6 +72,12 @@ while True:
                       print("Insufficient Funds")
                   else:   
                    user_data['bal'] -= amount
+                   trans_record ={"amount":amount,
+                                "status":"debit",
+                                "action":"withdraw",
+                                "trans_date":str(datetime.now())}
+                   transaction[acc_num].append(trans_record)
+                   
                    print("\nWithdrawal Successful!!!\nYour account balance is $%i"%user_data['bal'])
               
               elif user_select == "3":
@@ -70,14 +85,25 @@ while True:
                   other_acc = input("Enter Account number\n:>> ")
                   trans_amount = int(input("Enter Amount you want to Transfer\n:>> "))
                   
-                  beneficiary = bank_data.get(other_acc)
+                  beneficiary = customer.get(other_acc)
                 #   print(beneficiary)
-                  if beneficiary:
+                  if beneficiary and beneficiary['is_active']==True:
                     if trans_amount >= user_data['bal']:
                       print("Insufficient Funds")
                     else:   
                       user_data['bal'] -= trans_amount
+                      trans_record ={"amount":trans_amount,
+                                "status":"debit",
+                                "action":"transfer",
+                                "trans_date":str(datetime.now())}
+                      transaction[acc_num].append(trans_record)
+                      
                       beneficiary['bal'] += trans_amount
+                      trans_record ={"amount":trans_amount,
+                                "status":"credit",
+                                "action":"transfer",
+                                "trans_date":str(datetime.now())}
+                      transaction[other_acc].append(trans_record)
                       
                       print("\nTransfer Successful!!!\nYour account balance is $%i"%user_data['bal'])
                       
@@ -87,15 +113,30 @@ while True:
               elif user_select == "4":
                   print("******BALANCE******")
                   print("Your Balance is $%i"%user_data['bal'])        
-                
               elif user_select == "5":
+                  trans = transaction.get(acc_num)[-5:] 
+                  print(f"Current balance is: ${user_data['bal']}") 
+                  for tran in trans:
+                      print("="*8)
+                      print("Amount:", "$"+str(tran["amount"]))
+                      print("Status:", tran["status"].title())
+                      print("Action:", tran["action"].title())
+                      print("Date:", tran["trans_date"])
+                      print("="*8)
+              
+              elif user_select == "6":
+                print("You are about to deactivate your account. If you are sure about this then enter your pin to continue. Else press enter to skip.")
+                pin = input(">")
+                if user_data['pin'] == pin:
+                    user_data['is_active'] = False
+                    break
+              elif user_select == "7":
                 break   
               else:
                   print("Invalid Selection") 
         else:
             print("Invalid Details")        
-    
-      
+            
     elif user == 3:
         break
     else:
@@ -104,5 +145,5 @@ while True:
 print("**THANK YOU FOR BANKING WITH US**")  
  
 file = open("customers.txt", "w")
-bank_data = file.write(str(bank_data))
+customer = file.writelines([str(customer)+ "\n", str(transaction)])
 file.close()
