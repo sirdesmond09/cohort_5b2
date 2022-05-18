7e1640720a5e8a1e4213ae5d7fac02a817c233c8
sirdesmond09 <sirdesmond09@gmail.com>
2022-05-18 13:11:44 +0100

    models

--- 5b2_django/mysite/testapp/admin.py
+++ 5b2_django/mysite/testapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Item
 
 # Register your models here.
+
+# admin.site.register(Item)
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "date_added"]
+    list_filter = ["date_added"]
+    search_fields = ["name", "price"]
+    list_editable = ["price"]
--- 5b2_django/mysite/testapp/models.py
+++ 5b2_django/mysite/testapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    price = models.FloatField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+    # class Meta:
+    #     ordering = ["-id"]
+    
