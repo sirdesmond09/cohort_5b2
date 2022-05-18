9ebaf692006a08eaa688288f5c371c39e3f99e16
sirdesmond09 <sirdesmond09@gmail.com>
2022-05-18 11:17:05 +0100

    statistices

--- 5b2_django/mysite/testapp/urls.py
+++ 5b2_django/mysite/testapp/urls.py
@@ -5,6 +5,10 @@ urlpatterns = [
     path("test/",views.test_view),
     path("about_us/", views.about_us, name="about"),
     path("home/", views.html_home_page, name="home"),
-    path("contact/", views.contact_us, name="contact")
+    path("mean/", views.find_mean, name="mean"),
+    path("median/", views.median_value, name="median"),
+    path("std_dev/", views.standard_deviation, name="std_dev"),
+    path("skewness/", views.skewness, name="skewness"),
+    
 ]
 
--- 5b2_django/mysite/testapp/views.py
+++ 5b2_django/mysite/testapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from django.http.response import HttpResponse
-
+from statistics import mean, median, stdev
 
 def test_view(request):
     return HttpResponse("""<h1>This is a response</h1>
@@ -18,7 +18,7 @@ def html_home_page(request):
     }
     return render(request, "home.html", context)
 
-def contact_us(request):
+def find_mean(request):
     mean = None
     
     if request.method == "POST":
@@ -28,8 +28,63 @@ def contact_us(request):
         # return redirect("home")
         
     context = {
-        "section":"contact",
+        "section":"mean",
         "mean" : mean
     }
     
-    return render(request, "contact.html", context)
+    return render(request, "mean.html", context)
+
+
+
+
+def median_value(request):
+    med = None
+    
+    if request.method == "POST":
+        num = request.POST.get("numbers")
+        numbers = list(map(int, num.split(",")))
+        med = median(numbers)
+        
+    context = {
+        "section":"median",
+        "median" : med
+    }
+    
+    return render(request, "median.html", context)
+    
+
+def standard_deviation(request):
+    std_dev = None
+    
+    if request.method == "POST":
+        num = request.POST.get("numbers")
+        numbers = list(map(int, num.split(",")))
+        std_dev = round(stdev(numbers), 3)
+        
+    context = {
+        "section":"std_dev",
+        "std_dev" : std_dev
+    }
+    
+    return render(request, "std.html", context)
+
+
+def skewness(request):
+    skw = None
+    
+    if request.method == "POST":
+        num = request.POST.get("numbers")
+        numbers = list(map(int, num.split(",")))
+        mean_ = mean(numbers)
+        standard_dev = stdev(numbers)
+        n = len(numbers)
+        numerator = sum([(x-mean_)**3 for x in numbers])
+        denominator = (n-1) * (standard_dev**3)
+        skw =  numerator/denominator
+
+    context = {
+        "section":"skewness",
+        "skewness" : skw
+    }
+    
+    return render(request, "skewness.html", context)
