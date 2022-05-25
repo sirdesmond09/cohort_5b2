2c21150b30483bffc5bcf9157b30b03390e59faa
sirdesmond09 <sirdesmond09@gmail.com>
2022-05-25 15:54:12 +0100

    email sending

--- 5b2_django/class_blog/blog/views.py
+++ 5b2_django/class_blog/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -34,3 +36,34 @@ def post_detail(request, year, month, day, post):
     return render(request, 
                   'blog/post/detail.html',
                   context=data)
+    
+    
+    
+def post_share(request, post_id):
+     # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+     # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+        # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}\'s comments:\n{cd['comments']}"
+            
+            sender = "Basit from Bolt <admin@myblog.com>"
+            send_mail(subject, message, sender,
+            [cd['to']])
+            sent = True
+
+            
+    else:
+        form = EmailPostForm()
+        
+    data = {'post': post,
+            'form': form,
+            "sent":sent}
+    
+    return render(request, 'blog/post/share.html', context=data)
