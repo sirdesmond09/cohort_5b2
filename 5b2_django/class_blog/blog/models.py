76c92d465c65a0675b86bf25c87d96c830343d12
sirdesmond09 <sirdesmond09@gmail.com>
2022-05-27 14:58:41 +0100

    custom tags and filters

--- 5b2_django/class_blog/blog/models.py
+++ 5b2_django/class_blog/blog/models.py
@@ -18,8 +18,7 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,    choices=STATUS_CHOICES, default='draft')
-    
+    status = models.CharField(max_length=10,    choices=STATUS_CHOICES, default='draft')    
     
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
