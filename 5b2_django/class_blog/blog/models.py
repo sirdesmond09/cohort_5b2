938757294e1413940d9eb1db083135fa44ebf7cc
sirdesmond09 <sirdesmond09@gmail.com>
2022-05-27 13:16:59 +0100

    tags

--- 5b2_django/class_blog/blog/models.py
+++ 5b2_django/class_blog/blog/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 from .managers import PublishedManager
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -22,6 +23,7 @@ class Post(models.Model):
     
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
+    tags = TaggableManager()
     
     class Meta:
         ordering = ('-publish',)
--- 5b2_django/class_blog/blog/views.py
+++ 5b2_django/class_blog/blog/views.py
@@ -4,11 +4,19 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 # Create your views here.
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    
+    
     paginator = Paginator(object_list, 2) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -20,7 +28,8 @@ def post_list(request):
     # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
     data = {'posts': posts,
-            'page': page,}
+            'page': page,
+            'tag': tag}
     
     return render(request,'blog/post/list.html', context=data)
 
